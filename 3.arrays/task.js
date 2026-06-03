09abd598bd92da04b6ee188d6b4ca0c297a7fd7c
Dima <celischev85@gmail.com>
2026-06-03 22:27:34 +0300

    initial commit

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,21 @@
+
 function compareArrays(arr1, arr2) {
   
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  return arr1.every((element, index) => element === arr2[index]);
 }
 
+
 function getUsersNamesInAgeRange(users, gender) {
+  let filteredUsers = users.filter(user => user.gender === gender);
   
+  if (filteredUsers.length === 0) {
+    return 0;
+  }
+  
+  let totalAge = filteredUsers.reduce((acc, user) => acc + user.age, 0);
+  return totalAge / filteredUsers.length;
 }
